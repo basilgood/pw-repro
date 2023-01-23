29c5ac3a2f9cf777656dab993a167a4748214922
basilgood <elsile69@yahoo.com.com>
2023-01-23 23:32:38 +0200

    chore: test 2

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -2,6 +2,8 @@ const { test, expect } = require('@playwright/test');
 
 test('basic test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
-  await page.click('#myCheck');
-  await page.waitForSelector('"Checkbox is CHECKED!"');
+
+	await page.getByLabel('Color').fill('#ff00ff')
+
+	await page.keyboard.press('Enter');
 });
