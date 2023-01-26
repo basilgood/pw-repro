85473e603894f45a7c8c3e2b06a0beccc31e091c
basilgood <elsile69@yahoo.com.com>
2023-01-26 13:06:40 +0200

    chore: test 3

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -4,6 +4,7 @@ test('basic test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
 	await page.getByLabel('Color').fill('#ff00ff')
+  await expect(page.locator('#output')).toHaveText('#ff00ff')
 
-	await page.keyboard.press('Enter');
+	// await page.keyboard.press('Enter');
 });
